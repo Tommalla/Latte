05c71ff526825d75f1319b3a8c927531e5ecb587
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-12-17 11:29:15 +0100

    Improvements to typechecker.

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -3,7 +3,7 @@
 module TypeChecker where
 
 import Control.Monad.Trans.Except
-import Control.Monad.Trans.State
+import Control.Monad.State
 import qualified Data.Map as Map
 
 import AbsLatte
@@ -12,7 +12,8 @@ import LexLatte
 import ParLatte
 import PrintLatte
 
-type Env = Map.Map Ident Type
+data ExtType = NormalType Type | FunType Env Type [Arg] Block
+type Env = Map.Map Ident ExtType
 type Eval a = ExceptT String (State Env) a
 
 typeCheck :: Program -> Maybe String
@@ -21,4 +22,14 @@ typeCheck program = case fst (runState (runExceptT (checkProgram program)) Map.e
     (Left msg) -> Just msg
 
 checkProgram :: Program -> Eval ()
-checkProgram prog = return ()
+checkProgram (Program topDefs) = do
+    mapM_ (checkTopDef) topDefs
+    -- TODO find main and typecheck run
+
+checkTopDef :: TopDef -> Eval ()
+checkTopDef (FnDef retType ident args block) = checkFnDef retType ident args block
+
+checkFnDef :: Type -> Ident -> [Arg] -> Block -> Eval ()
+checkFnDef retType ident args block = do
+    env <- get
+    put $ Map.insert ident (FunType env retType args block) env
