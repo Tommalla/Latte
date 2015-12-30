d3c11437d57aeb739a755cd4c2e6b7805a34d951
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2015-12-30 16:55:06 +0100

    Added typechecking for most expressions, including function invocation.
    Rebuilt the whole environment, adding PEnv for functions, reworked the invocation, simplified the code.
    
    Refs #1.

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -12,18 +12,16 @@ import LexLatte
 import ParLatte
 import PrintLatte
 
-data ExtType = NormalType Type | FunType Env Type [Arg] Block
+data TypecheckError = ExactError String | IdentNotFound Ident | UnexpectedRetType Type Type | UnexpectedType Type Type | ArgMismatch Arg | WrongArgsNo Int Int |
+        NotNumeric Type | NotBoolConvertible Type | NotAnArray Ident
     deriving (Eq, Show)
-data TypecheckError = ExactError String | IdentNotFound Ident | UnexpectedExtType Ident ExtType | 
-        UnexpectedRetType Type Type | UnexpectedType Type Type | ArgMismatch Arg | WrongArgsNo Int Int |
-        NotNumeric Type | NotBoolConvertible Type
-
-    deriving (Eq, Show)
-type Env = Map.Map Ident ExtType
-type Eval a = ExceptT TypecheckError (State Env) a
+type FunType = (Env, Type, [Arg], Block)
+type PEnv = Map.Map Ident FunType
+type Env = Map.Map Ident Type
+type Eval a = ExceptT TypecheckError (State (Env, PEnv)) a
 
 typeCheck :: Program -> Maybe String
-typeCheck program = case fst (runState (runExceptT (checkProgram program)) Map.empty) of
+typeCheck program = case fst (runState (runExceptT (checkProgram program)) (Map.empty, Map.empty)) of
     (Right _) -> Nothing
     (Left exc) -> case exc of
         (ExactError msg) -> Just msg
@@ -32,57 +30,60 @@ typeCheck program = case fst (runState (runExceptT (checkProgram program)) Map.e
 checkProgram :: Program -> Eval ()
 checkProgram (Program topDefs) = do
     mapM_ (checkTopDef) topDefs
-    mainType <- getExtType (Ident "main")
-    case mainType of
-        (FunType _ retType args block) -> do
-            env <- get  -- We need current env b/c of declarations
-            -- TODO no top-level args for now
-            checkFnInv retType args [] block
-        _ -> throwE (ExactError $ "'main' is not a function. The actual type is: " ++ (show mainType))
+    checkFnApp (Ident "main") []  -- TODO toplevel args still unsupported
+    return ()
 
 checkTopDef :: TopDef -> Eval ()
 checkTopDef (FnDef retType ident args block) = checkFnDef retType ident args block
 
 checkFnDef :: Type -> Ident -> [Arg] -> Block -> Eval ()
 checkFnDef retType ident args block = do
-    env <- get
-    put $ Map.insert ident (FunType env retType args block) env
+    (env, penv) <- get
+    put $ (env, Map.insert ident (env, retType, args, block) penv)
 
-getExtType :: Ident -> Eval ExtType
-getExtType ident = do
-    env <- get
-    case Map.lookup ident env of
-        (Just t) -> return t
+getFunction :: Ident -> Eval FunType
+getFunction ident = do
+    (_, penv) <- get
+    case Map.lookup ident penv of
+        (Just fn) -> return fn
         Nothing -> throwE (IdentNotFound ident)
 
 getType :: Ident -> Eval Type
 getType ident = do
-    extType <- getExtType ident
-    case extType of
-        (NormalType t) -> return t
-        _ -> throwE (UnexpectedExtType ident extType)
+    (env, _) <- get
+    case Map.lookup ident env of
+        (Just t) -> return t
+        Nothing -> throwE (IdentNotFound ident)
 
-checkFnInv :: Type -> [Arg] -> [Ident] -> Block -> Eval ()
-checkFnInv retType args passedIdents block = do
-    env <- get
-    bindArgs args passedIdents
-    actRetType <- checkBlock block
-    put env
-    if retType == actRetType then
-        return ()
-    else throwE (UnexpectedRetType retType actRetType)
+checkFnApp :: Ident -> [Type] -> Eval Type
+checkFnApp ident passedTypes = do
+    (env, retType, args, block) <- getFunction ident
+    (oldEnv, penv) <- get
+    put (env, penv)
+    res <- checkFnInv retType args passedTypes block
+    put (oldEnv, penv)
+    return res
+    where
+        checkFnInv :: Type -> [Arg] -> [Type] -> Block -> Eval Type
+        checkFnInv retType args passedTypes block = do
+            mem <- get
+            bindArgs args passedTypes
+            actRetType <- checkBlock block
+            put mem
+            if retType == actRetType then
+                return retType
+            else throwE (UnexpectedRetType retType actRetType)
 
-bindArgs :: [Arg] -> [Ident] -> Eval ()
-bindArgs args passedIdents = do
-    let (argsCnt, passedCnt) = (length args, length passedIdents)
+bindArgs :: [Arg] -> [Type] -> Eval ()
+bindArgs args passedTypes = do
+    let (argsCnt, passedCnt) = (length args, length passedTypes)
     if argsCnt == passedCnt then
-        mapM_ (\(arg, ident) -> bindArg arg ident) $ zip args passedIdents
+        mapM_ (\(arg, t) -> bindArg arg t) $ zip args passedTypes
     else throwE (WrongArgsNo argsCnt passedCnt)
 
-bindArg :: Arg -> Ident -> Eval ()
-bindArg (Arg t ident) passedIdent = do
-    passedT <- getType passedIdent
-    if passedT == t then
+bindArg :: Arg -> Type -> Eval ()
+bindArg (Arg t ident) passedType = do
+    if passedType == t then
         declare t ident
     else throwE (ArgMismatch (Arg t ident))
 
@@ -101,9 +102,9 @@ checkBlock (Block stmts) = do
 checkStmt :: Stmt -> Eval (Maybe Type)
 checkStmt Empty = return Nothing
 checkStmt (BStmt block) = do
-    env <- get
+    mem <- get
     res <- checkBlock block
-    put env
+    put mem
     return (Just res)
 checkStmt (Decl t items) = do
     mapM_ (declareItem t) items
@@ -112,7 +113,7 @@ checkStmt (Ass ident expr) = do
     l <- getType ident
     r <- checkExpr expr
     if l == r then
-        return (Just l)
+        return Nothing
     else throwE (UnexpectedType l r)
 checkStmt (Incr ident) = do
     t <- getType ident
@@ -128,17 +129,17 @@ checkStmt (Cond expr stmt) = checkStmt (CondElse expr stmt Empty)
 checkStmt (CondElse expr stmtTrue stmtFalse) = do
     condT <- checkExpr expr
     if isBoolConvertible condT then do
-        env <- get
+        mem <- get
         checkStmt stmtTrue
-        put env
+        put mem
         checkStmt stmtFalse
-        put env
+        put mem
         return Nothing
     else throwE (NotBoolConvertible condT)
 checkStmt (While expr stmt) = checkStmt (Cond expr stmt)
 checkStmt (SExp expr) = do
-    res <- checkExpr expr
-    return (Just res)
+    checkExpr expr
+    return Nothing
 
 declareItem :: Type -> Item -> Eval ()
 declareItem t (NoInit ident) = declare t ident
@@ -150,8 +151,8 @@ declareItem t (Init ident expr) = do
 
 declare :: Type -> Ident -> Eval ()
 declare t ident = do
-    env <- get
-    put $ Map.insert ident (NormalType t) env
+    (env, penv) <- get
+    put $ (Map.insert ident t env, penv)
 
 isNumeric :: Type -> Bool
 isNumeric Int = True
@@ -162,4 +163,19 @@ isBoolConvertible Bool = True
 isBoolConvertible t = isNumeric t
 
 checkExpr :: Expr -> Eval Type
+checkExpr (ENewArr t _) = return (Array t)
+checkExpr (EArrElem ident _) = do
+    t <- getType ident
+    case t of
+        (Array resT) -> return resT
+        _ -> throwE (NotAnArray ident)
+checkExpr (EVar ident) = do
+    t <- getType ident
+    return t
+checkExpr (ELitInt _) = return Int
+checkExpr ELitTrue = return Bool
+checkExpr ELitFalse = return Bool
+checkExpr (EApp ident exprs) = do 
+   passedTypes <- mapM (checkExpr) exprs
+   checkFnApp ident passedTypes
 checkExpr _ = return Int
